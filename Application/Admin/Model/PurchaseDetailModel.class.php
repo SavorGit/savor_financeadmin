51abae1e3e3c628b1dd1e2541902bfa53b0a9e35
zhangyingtao75144 <125929019@qq.com>
2022-04-07 17:31:45 +0800

    采购model

--- Application/Admin/Model/PurchaseDetailModel.class.php
+++ Application/Admin/Model/PurchaseDetailModel.class.php
@@ -1,9 +1,28 @@
 <?php
 namespace Admin\Model;
-
+use Common\Lib\Page;
+use Think\Model;
 class PurchaseDetailModel extends BaseModel{
 	protected $tableName='finance_purchase_detail';
 
-    
+	public function getList($fields,$where, $order='a.id desc', $start=0,$size=5){
+	    $list = $this->alias('a')
+	    ->join('savor_finance_goods g on a.goods_id = g.id','left')
+	    ->join('savor_finance_category c on g.category_id=c.id','left')
+	    ->join('savor_finance_unit  u on a.unit_id  = u.id','left')
+	    ->field($fields)
+	    ->where($where)
+	    ->order($order)
+	    ->limit($start,$size)
+	    ->select();
+	    
+	    $count = $this->alias('a')
+	    ->where($where)
+	    ->count();
+	    $objPage = new Page($count,$size);
+	    $show = $objPage->admin_page();
+	    $data = array('list'=>$list,'page'=>$show);
+	    return $data;
+	}
 
 }
--- Application/Admin/Model/PurchaseModel.class.php
+++ Application/Admin/Model/PurchaseModel.class.php
@@ -1,9 +1,27 @@
 <?php
 namespace Admin\Model;
-
+use Common\Lib\Page;
+use Think\Model;
 class PurchaseModel extends BaseModel{
 	protected $tableName='finance_purchase';
 
-    
+	public function getList($fields,$where, $order='a.id desc', $start=0,$size=5){
+	    $list = $this->alias('a')
+	    ->join('savor_finance_department d on a.department_id = d.id','left')
+	    ->join('savor_finance_supplier   s on a.supplier_id   = s.id','left')
+	    ->field($fields)
+	    ->where($where)
+	    ->order($order)
+	    ->limit($start,$size)
+	    ->select();
+	    
+	    $count = $this->alias('a')
+	    ->where($where)
+	    ->count();
+	    $objPage = new Page($count,$size);
+	    $show = $objPage->admin_page();
+	    $data = array('list'=>$list,'page'=>$show);
+	    return $data;
+	}
 
 }
