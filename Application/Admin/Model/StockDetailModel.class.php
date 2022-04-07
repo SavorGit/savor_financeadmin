4f1fda4f914c9a1fa20e9631eb8e1ed449cc86b0
liubin0125 <liubin-0125@163.com>
2022-04-07 17:56:32 +0800

    新增入库管理

---
+++ Application/Admin/Model/StockDetailModel.class.php
@@ -0,0 +1,27 @@
+<?php
+namespace Admin\Model;
+use Common\Lib\Page;
+
+class StockDetailModel extends BaseModel{
+	protected $tableName='finance_stock_detail';
+
+    public function getList($fields,$where, $order='a.id desc', $start=0,$size=5){
+        $list = $this->alias('a')
+            ->join('savor_finance_goods goods on a.goods_id=goods.id','left')
+            ->join('savor_finance_category cate on goods.category_id=cate.id','left')
+            ->field($fields)
+            ->where($where)
+            ->order($order)
+            ->limit($start,$size)
+            ->select();
+
+        $count = $this->alias('a')
+            ->where($where)
+            ->count();
+        $objPage = new Page($count,$size);
+        $show = $objPage->admin_page();
+        $data = array('list'=>$list,'page'=>$show);
+        return $data;
+    }
+
+}
