ca5e131d20c6b441af7c872ece7427376af64c66
liubin0125 <liubin-0125@163.com>
2022-04-07 17:30:46 +0800

    新增相关类型

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -100,6 +100,9 @@ $config['MANGER_STATE'] = array(
     '2'=>'审核通过',
     '3'=>'审核不通过',
 );
+$config['STOCK_IN_TYPES'] = array('11'=>'采购','12'=>'调拨','13'=>'酒楼结余');
+$config['STOCK_OUT_TYPES'] = array('21'=>'调拨','22'=>'团购售卖','23'=>'酒楼备货');
+
 $config['MANGER_KEY'] = array(
     'system'=>'系统管理',
     'baseset'=>'基础设置',
