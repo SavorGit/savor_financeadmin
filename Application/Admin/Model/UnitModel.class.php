a2c67357b6d20bd192d5ee79b4889388e57917a5
liubin0125 <liubin-0125@163.com>
2022-05-24 17:40:22 +0800

    修改根据商品类型获取单位

--- Application/Admin/Model/UnitModel.class.php
+++ Application/Admin/Model/UnitModel.class.php
@@ -9,8 +9,10 @@ class UnitModel extends BaseModel{
         $sql_unit = "select * from savor_finance_unit where id in(
         select unit_stock.unit_id from (select unit_id,sum(amount) as amount from savor_finance_stock_detail where goods_id={$goods_id} group by unit_id) as unit_stock
         where unit_stock.amount>0)";
-        */
+
         $sql_unit = "select * from savor_finance_unit where id in(select unit_id from savor_finance_stock_detail where goods_id={$goods_id} group by unit_id)";
+        */
+        $sql_unit = "select * from savor_finance_unit where category_id=(select category_id from savor_finance_goods where id={$goods_id}) and status=1";
         $res = $this->query($sql_unit);
         return $res;
     }
