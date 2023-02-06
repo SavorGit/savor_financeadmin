92b28dc82579da0773ad5687af8201cf56add358
zhangyingtao75144 <125929019@qq.com>
2023-02-06 13:34:45 +0800

    数据查询-导表

--- Application/Datacenter/Controller/SaleissueController.class.php
+++ Application/Datacenter/Controller/SaleissueController.class.php
@@ -53,5 +53,70 @@ class SaleissueController extends BaseController {
         $this->assign('numPerPage',$size);
         $this->display();
     }
+    /**
+     * @desc 数据查询 销售出库单汇总表
+     */
+    public function datasummary(){
+        $page = I('pageNum',1);
+        $size   = I('numPerPage',50);
+        
+        
+        $start  = ($page-1) * $size;
+        
+        $order = I('_order','a.id');
+        
+        $this->assign('_order',$order);
+        $sort = I('_sort','desc');
+        $this->assign('_sort',$sort);
+        $orders = $order.' '.$sort;
+        
+        $start_date = I('start_date','');
+        $end_date   = I('end_date','');
+        $start_date =  !empty($start_date) ? $start_date: date('Y-m-d',strtotime('-7 days'));
+        $end_date   =  !empty($end_date) ? $end_date: date('Y-m-d');
+        
+        /*$where = [];
+        $where['a.add_time'] = array(array('EGT',$start_date.' 00:00:00'),array('ELT',$end_date.' 23:59:59'));
+        
+        $fields = "a.hotel_id,hotel.name hotel_name";
+        $group  = "a.hotel_id";
+        $m_sale = new \Admin\Model\SaleModel();
+        $list = $m_sale->alias('a')
+                         ->join('savor_hotel hotel on a.hotel_id = hotel.id','left')
+                         
+                         ->field($fields)
+                         ->where($where)
+                         ->order($orders)
+                         ->group($group)
+                         ->limit($start,$size)
+                         ->select();
+        $sale_type_arr = $this->sale_type_arr;
+        foreach($list as $key=>$v){
+            
+            foreach($sale_type_arr as $kk=>$vv){
+                $ret = $m_sale->alias('a')
+                              ->join('savor_hotel hotel on a.hotel_id = hotel.id','left')
+                              ->join('savor_finance_goods hotel on a.hotel_id = hotel.id','left')
+            }
+        }
+        $count =  $m_sale->alias('a')
+                         ->join('savor_hotel hotel on a.hotel_id = hotel.id','left')
+                         ->field($fields)
+                         ->where($where)
+                         ->order($orders)
+                         ->group($group)
+                         ->select();
+        $objPage = new Page($count,$size);
+        $show = $objPage->admin_page();
+        $result = array('list'=>$list,'page'=>$show);
+        
+        $this->assign('list', $result['list']);
+        $this->assign('page',  $result['page']);*/
+        $this->assign('start_date',$start_date);
+        $this->assign('end_date',$end_date);
+        $this->assign('pageNum',$page);
+        $this->assign('numPerPage',$size);
+        $this->display();
+    }
     
 }
