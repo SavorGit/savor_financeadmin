1d2f4f21b84b91dac1aedba7137bf65864082d87
liubin0125 <liubin-0125@163.com>
2023-04-11 18:54:07 +0800

    增加收款列表关联销售出库单

--- Application/Admin/Model/SalePaymentModel.class.php
+++ Application/Admin/Model/SalePaymentModel.class.php
@@ -1,8 +1,35 @@
 <?php
 namespace Admin\Model;
+use Common\Lib\Page;
 
 class SalePaymentModel extends BaseModel{
 
     protected $tableName='finance_sale_payment';
 
+    public function getList($fields,$where,$orderby,$start=0,$size=0){
+        if($start >= 0 && $size){
+            $list = $this->alias('a')
+                ->join('savor_hotel hotel on a.hotel_id=hotel.id','left')
+                ->field($fields)
+                ->where($where)
+                ->order($orderby)
+                ->limit($start,$size)
+                ->select();
+            $count = $this->alias('a')
+                ->join('savor_hotel hotel on a.hotel_id=hotel.id','left')
+                ->where($where)
+                ->count();
+            $objPage = new Page($count,$size);
+            $show = $objPage->admin_page();
+            $data = array('list'=>$list,'page'=>$show,'total'=>$count);
+        }else{
+            $data = $this->alias('a')
+                ->join('savor_hotel hotel on a.hotel_id=hotel.id','left')
+                ->field($fields)
+                ->where($where)
+                ->order($orderby)
+                ->select();
+        }
+        return $data;
+    }
 }
