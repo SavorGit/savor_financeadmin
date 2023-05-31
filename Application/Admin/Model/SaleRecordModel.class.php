4daa93a473fc12d891a506497893a965c3559621
liubin0125 <liubin-0125@163.com>
2023-05-31 17:37:50 +0800

    新增盘点异常列表数据

--- Application/Admin/Model/SaleRecordModel.class.php
+++ Application/Admin/Model/SaleRecordModel.class.php
@@ -17,4 +17,29 @@ class SaleRecordModel extends BaseModel{
         return $data;
     }
 
+    public function getCheckDataList($fields,$where,$orderby,$start=0,$size=0){
+        if($start >= 0 && $size){
+            $list = $this->alias('record')
+                ->field($fields)
+                ->join('savor_hotel hotel on record.signin_hotel_id=hotel.id','left')
+                ->join('savor_ops_staff staff on record.ops_staff_id=staff.id','left')
+                ->join('savor_sysuser sysuser on staff.sysuser_id=sysuser.id','left')
+                ->where($where)
+                ->order($orderby)
+                ->limit($start,$size)
+                ->select();
+            $count = $this->alias('record')
+                ->field($fields)
+                ->join('savor_hotel hotel on record.signin_hotel_id=hotel.id','left')
+                ->join('savor_ops_staff staff on record.ops_staff_id=staff.id','left')
+                ->join('savor_sysuser sysuser on staff.sysuser_id=sysuser.id','left')
+                ->where($where)->count();
+            $objPage = new Page($count,$size);
+            $show = $objPage->admin_page();
+            $data = array('list'=>$list,'page'=>$show,'total'=>$count);
+        }else{
+            $data = $this->field($fields)->where($where)->order($orderby)->select();
+        }
+        return $data;
+    }
 }
