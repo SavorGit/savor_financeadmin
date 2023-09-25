ec200c8903b9c2f5b9c22ea46d838d0092de3ba5
liubin0125 <liubin-0125@163.com>
2023-09-25 16:52:24 +0800

    销售出库单列表增加团购数据

--- Application/Datacenter/Controller/SaleissueController.class.php
+++ Application/Datacenter/Controller/SaleissueController.class.php
@@ -18,18 +18,12 @@ class SaleissueController extends BaseController {
         $start_date =  !empty($start_date) ? $start_date: date('Y-m-d',strtotime('-7 days'));
         $end_date   =  !empty($end_date) ? $end_date: date('Y-m-d');
         
-        $where = [];
+        $where = array();
         $where['a.add_time'] = array(array('EGT',$start_date.' 00:00:00'),array('ELT',$end_date.' 23:59:59'));
-        /*$fields = "a.add_time,a.id,case a.type when 1 then '餐厅销售' when 2 then '团购售卖' when 3 then '其他售卖' end as type,
-                   a.idcode,area.region_name,a.hotel_id,hotel.name hotel_name,goods.barcode,
-                   goods.name goods_name,unit.name unit_name,spe.name spe_name,a.settlement_price,
-                   a.cost_price,a.settlement_price-a.cost_price as profit ,
-                   spr.add_time pay_time,spr.pay_money,a.settlement_price-spr.pay_money uncollected_money,
-                   a.invoice_time,a.invoice_money,sysuser.remark,user.nickName,user.name";*/
         $fields = "a.add_time,a.id,a.type,record.wo_reason_type,
                    a.idcode,area.region_name,a.hotel_id,hotel.name hotel_name,goods.barcode,
                    goods.name goods_name,unit.name unit_name,spe.name spe_name,a.settlement_price,
-                   a.cost_price,a.settlement_price-a.cost_price as profit ,
+                   a.cost_price,
                    a.invoice_time,a.invoice_money,sysuser.remark,user.nickName,user.name,ar.region_name tg_region_name";
         $m_sale = new \Admin\Model\SaleModel();
         $result = $m_sale->getAllList($fields, $where, $orders, $start, $size);
@@ -39,11 +33,19 @@ class SaleissueController extends BaseController {
         foreach($result['list'] as $key=>$v){
             if($v['type']==1){
                 $type = $all_stock_types[$v['wo_reason_type']];
+                $amount = 1;
             }else{
+                $all_idcodes = explode("\n",$v['idcode']);
+                $amount = count($all_idcodes);
                 $type = $all_sale_types[$v['type']];
                 $result['list'][$key]['unit_name'] = '瓶';
                 $result['list'][$key]['region_name'] = $v['tg_region_name'];
             }
+            $profit = $v['settlement_price']-$v['cost_price']*$amount;
+
+            $result['list'][$key]['amount'] = $amount;
+            $result['list'][$key]['cost_price'] = $v['cost_price']*$amount;
+            $result['list'][$key]['profit'] = $profit;
             $result['list'][$key]['type'] = $type;
             $rts = $m_sale_payment_record->where(array('sale_id'=>$v['id']))->field('add_time as  pay_time,pay_money')->order('add_time desc')->select();
             if(empty($v['name'])){
