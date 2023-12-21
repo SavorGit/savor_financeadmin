ae3feb4d5d8a89a422304eb58b7e09bae779e459
liubin0125 <liubin-0125@163.com>
2023-12-21 17:46:49 +0800

    更新u8接口类

--- Application/Common/Lib/U8cloud.class.php
+++ Application/Common/Lib/U8cloud.class.php
@@ -2,7 +2,7 @@
 namespace Common\Lib;
 class U8cloud {
 
-    public function apiquery($api,$method,$params){
+    public function apiquery($api,$method,$params,$trantype=''){
         $u8_conf = C('U8_CONFIG');
         $api_host = $u8_conf['api_host'];
         $system = $u8_conf['system'];
@@ -15,6 +15,9 @@ class U8cloud {
             "password: $password",
             "system: $system",
         );
+        if(!empty($trantype)){
+            $header_info[]="trantype: $trantype";
+        }
         $GLOBALS['HEADERINFO'] = $header_info;
         $api_url = $api_host.$api;
         $res = '';
@@ -33,11 +36,99 @@ class U8cloud {
         }
         return array('url'=>$api_url,'result'=>$res);
     }
-    //银行账户添加
-    public function addBankAccountInfo($params){
-        $api = '/u8cloud/api/uapbd/bankaccount/insert';
+    //部门档案添加
+    public function addDepartmentInfo($params){
+        $api = '/u8cloud/api/uapbd/bddept/save';
         $method = 'post';
         $res = $this->apiquery($api,$method,$params);
+        return $res;
+    }
+    //部门档案修改
+    public function editDepartmentInfo($params){
+        $api = '/u8cloud/api/uapbd/bddept/update';
+        $method = 'post';
+        $res = $this->apiquery($api,$method,$params);
+        return $res;
+    }
+
+    //部门人员新增、修改
+    public function saveDepartmentUser($params){
+        $api = '/u8cloud/api/uapbd/bdpsn/save';
+        $method = 'post';
+        $res = $this->apiquery($api,$method,$params);
+        return $res;
+    }
+    //部门人员删除
+    public function delDepartmentUser($params){
+        $api = '/u8cloud/api/uapbd/bdpsn/delete';
+        $method = 'post';
+        $res = $this->apiquery($api,$method,$params);
+        return $res;
+    }
+    //新增存货分类
+    public function addGoodsBrand($params){
+        $api = '/u8cloud/api/uapbd/bdinvcl/save';
+        $method = 'post';
+        $res = $this->apiquery($api,$method,$params);
+        return $res;
+    }
+    //编辑存货分类
+    public function editGoodsBrand($params){
+        $api = '/u8cloud/api/uapbd/bdinvcl/update';
+        $method = 'post';
+        $res = $this->apiquery($api,$method,$params);
+        return $res;
+    }
+    //新增存货档案
+    public function addGoods($params){
+        $api = '/u8cloud/api/uapbd/invbasdoc/insert';
+        $method = 'post';
+        $res = $this->apiquery($api,$method,$params);
+        return $res;
+    }
+    //新增存货档案
+    public function editGoods($params){
+        $api = '/u8cloud/api/uapbd/invbasdoc/update';
+        $method = 'post';
+        $res = $this->apiquery($api,$method,$params);
+        return $res;
+    }
+    //新增会计科目
+    public function addAccsubj($params){
+        $api = '/u8cloud/api/uapbd/accsubj/insert';
+        $method = 'post';
+        $res = $this->apiquery($api,$method,$params);
+        return $res;
+    }
+    //编辑会计科目
+    public function editAccsubj($params){
+        $api = '/u8cloud/api/uapbd/accsubj/update';
+        $method = 'post';
+        $res = $this->apiquery($api,$method,$params);
+        return $res;
+    }
+    //客商基本档案新增
+    public function addCustdoc($params){
+        $api = '/u8cloud/api/uapbd/custdoc/insert';
+        $method = 'post';
+        $trantype = 'code';
+        $res = $this->apiquery($api,$method,$params,$trantype);
+        return $res;
+    }
+    //客商基本档案修改
+    public function editCustdoc($params){
+        $api = '/u8cloud/api/uapbd/custdoc/update';
+        $method = 'post';
+        $trantype = 'code';
+        $res = $this->apiquery($api,$method,$params,$trantype);
+        return $res;
+    }
+    //凭证新增
+    public function addVoucher($params){
+        $api = '/u8cloud/api/gl/voucher/insert';
+        $method = 'post';
+        $trantype = 'code';
+        $res = $this->apiquery($api,$method,$params,$trantype);
         return $res;
     }
 }
