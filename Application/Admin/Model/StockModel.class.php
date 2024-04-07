b7c163f69fe52d28f36757f185a3569abb80c126
liubin0125 <liubin-0125@163.com>
2024-04-07 17:18:13 +0800

    增加特殊酒楼限制放开

--- Application/Admin/Model/StockModel.class.php
+++ Application/Admin/Model/StockModel.class.php
@@ -24,6 +24,10 @@ class StockModel extends BaseModel{
     }
 
     public function checkHotelThreshold($hotel_id){
+        $ts_hotels = array(43686,43475,24041,46826,43015,43427,43688,24705,42425,43117);
+        if(in_array($hotel_id,$ts_hotels)){
+            return 1;
+        }
         $m_sale = new \Admin\Model\SaleModel();
         $fileds = 'sum(a.settlement_price) as money';
         $where = array('a.hotel_id'=>$hotel_id,'a.ptype'=>0,'record.type'=>7,'record.wo_reason_type'=>1,'record.wo_status'=>2);
